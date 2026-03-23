5950bc76c3aa0d4c3c5c5d9f49f76ae29ce6b492
Vaibhavpandey8 <96644799+Vaibhavpandey8@users.noreply.github.com>
2026-03-23 22:49:57 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -42,7 +42,7 @@ const Signup = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/users/register", {
+      const res = await fetch("https://omnifood-backend-fc4f.onrender.com/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
